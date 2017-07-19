f2ac7ad67895ebf79391390f7b618b63fcf65f0f
RationalAnimal <RationalAnimal@users.noreply.github.com>
2017-07-19 07:19:08 -0400

    updated tests

--- test/recognizer.js
+++ test/recognizer.js
@@ -981,6 +981,18 @@ describe("vui-ad-hoc-alexa-recognizer", function() {
           }});
     });
 
+    it("verify simple utterance with an AMAZON.Airport slot and INCLUDE_WILDCARD_MATCH flag matches matches", function() {
+      let result = recognizer.Recognizer.matchText("Blah Blah Airport is an unknown airport");
+      expect(result).to.eql(
+        {"name": "AirportIntent",
+          "slots": {
+            "AirportSlot": {
+              "name": "AirportSlot",
+              "value": "Blah Blah Airport"
+            }
+          }});
+    });
+
     it("verify simple utterance with an AMAZON.Airport slot and COUNTRY flag matches matches", function() {
       let result = recognizer.Recognizer.matchText("Birmingham Shuttlesworth is a united states airport");
       expect(result).to.eql(
