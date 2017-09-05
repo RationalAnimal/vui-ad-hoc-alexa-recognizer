3331a5c34b5f592416aa335d0cff6809e9edac80
RationalAnimal <RationalAnimal@users.noreply.github.com>
2017-09-05 14:56:12 -0400

    refactoring accessor to move common functions into utils.js

---
+++ builtinstateaccessors/utils.js
@@ -0,0 +1,69 @@
+/*
+ @author Ilya Shubentsov
+
+ MIT License
+
+ Copyright (c) 2017 Ilya Shubentsov
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+'use strict'
+
+let _ensureSubfieldsPresent = function(objectToUpdate, keys){
+  if(typeof objectToUpdate === "undefined" || objectToUpdate === null){
+    return;
+  }
+  if(typeof keys === "undefined" || keys === null){
+    return;
+  }
+  let keyArray = [];
+  if(typeof keys === "string"){
+    keyArray = keys.split(".");
+  }
+  else if(Array.isArray(keys)){
+    for(let i = 0; i < keys.length; i ++){
+      if(typeof keys[i] === "string"){
+        keyArray = keyArray.concat(keys.split("."));
+      }
+    }
+  }
+  if(keyArray.length > 0){
+    // First ensure that we have all the "sub" fields
+    let result = objectToUpdate;
+    for(let i = 0; i < keyArray.length; i++){
+      let scratch = result[keyArray[i]];
+      if(typeof scratch === "undefined" || scratch === null){
+        // We now need to add all the missing fields
+        result[keyArray[i]] = {};
+        result = result[keyArray[i]];
+      }
+      else {
+        result = scratch;
+      }
+    }
+  }
+};
+
+let utils = class {
+};
+
+utils.ensureSubfieldsPresent = _ensureSubfieldsPresent;
+utils.prototype.ensureSubfieldsPresent = _ensureSubfieldsPresent;
+
+module.exports = utils;
