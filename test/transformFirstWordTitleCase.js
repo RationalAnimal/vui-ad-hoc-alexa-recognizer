ca4d449e874a51dc54d417176381f5ec90b4d174
RationalAnimal <RationalAnimal@users.noreply.github.com>
2017-09-16 13:27:41 -0400

    deleting all the source

--- test/transformFirstWordTitleCase.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-module.exports = function(value, intentName, slotName){
-	if(typeof value != "undefined" && value != null){
-		let returnValue = value.toLowerCase();
-		returnValue = returnValue.slice(0, 1).toUpperCase() + returnValue.slice(1);
-		return returnValue;
-	}
-	return;
-};
--- test/transformSome.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-module.exports = function(value, intentName, slotName){
-	if(typeof value != "undefined" && value != null){
-		let returnValue = value.replace(/\s/ig,"_");
-		return returnValue;
-	}
-	return;
-};
