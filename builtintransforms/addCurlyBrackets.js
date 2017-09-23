d657c3be2739f527d4c24515a11cc1ed17fd9b59
RationalAnimal <RationalAnimal@users.noreply.github.com>
2017-09-23 19:34:33 -0400

    added another built in transform function

---
+++ builtintransforms/addCurlyBrackets.js
@@ -0,0 +1,8 @@
+'use strict'
+module.exports = function(value, intentName, slotName, slotType){
+	if(typeof value != "undefined" && value != null){
+		let returnValue = "{" + value + "}";
+		return returnValue;
+	}
+	return;
+};
