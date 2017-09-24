2849242dd49abac109f33cb6eb638d3d335d895e
RationalAnimal <RationalAnimal@users.noreply.github.com>
2017-09-24 00:09:38 -0400

    more transform functions

--- builtintransforms/removeNonWordCharacters.js
+++ builtintransforms/removeNonWordCharacters.js
@@ -1,16 +1,16 @@
 'use strict'
 /**
- * Removes non alphanumeric characters from the provided value
- * @param value - from which to remove non alphanumeric characters
+ * Removes non word characters from the provided value
+ * @param value - from which to remove non word characters
  * @param intentName - the intent name, ignored here since this is a built in transform function.
  * @param slotName - the slot name, ignored here since this is a built in transform function.
  * @param slotType - the slot type, ignored here since this is a built in transform function.
- * @returns {*} either either the value with all the non alphanumeric characters removed or undefined if
+ * @returns {*} either either the value with all the non word characters removed or undefined if
  * the input is undefined or null
  */
 module.exports = function(value, intentName, slotName, slotType){
 	if(typeof value !== "undefined" && value !== null){
-		let returnValue = JSON.stringify(value).replace(/[^0-9a-z]/gi, '');
+		let returnValue = JSON.stringify(value).replace(/\W/g, '');
 		return returnValue;
 	}
 	return value;
