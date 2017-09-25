7e9be7a67d6cae19f719303754a7f5f18d89054d
RationalAnimal <RationalAnimal@users.noreply.github.com>
2017-09-25 17:19:05 -0400

    continuing to lint

--- responder.js
+++ responder.js
@@ -23,7 +23,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-'use strict'
+"use strict";
 
 let responder = {};
 /*
@@ -55,22 +55,22 @@ let _produceResult = function(matchedIntent, stateAccessor, stateSelectors, resp
     let updatedStateSelectors = [].concat(stateSelectors);
 
     switch(updateRule){
-      case "setTo":
-        stateAccessor.setStateChain(updatedStateSelectors, responderSpec.updateState.directValue);
-        break;
-      case "mergeReplace":
-        updatedStateSelectors.push(responderSpec.updateState.updateSelector);
-        stateAccessor.mergeReplaceState(updatedStateSelectors, responderSpec.updateState.directValue);
-        break;
-      default:
-        // TODO Do nothing for now, revisit later
-        break;
+    case "setTo":
+      stateAccessor.setStateChain(updatedStateSelectors, responderSpec.updateState.directValue);
+      break;
+    case "mergeReplace":
+      updatedStateSelectors.push(responderSpec.updateState.updateSelector);
+      stateAccessor.mergeReplaceState(updatedStateSelectors, responderSpec.updateState.directValue);
+      break;
+    default:
+      // TODO Do nothing for now, revisit later
+      break;
     }
   }
   if(typeof responderSpec.result !== "undefined" && responderSpec.result !== null){
     if(typeof responderSpec.result.functionSource === "string"){
       try{
-        let scratchFunc = new Function('intent', 'stateAccessor', 'selectorArray', 'state', responderSpec.result.functionSource);
+        let scratchFunc = new Function("intent", "stateAccessor", "selectorArray", "state", responderSpec.result.functionSource);
         let result = scratchFunc(matchedIntent, stateAccessor, stateSelectors);
         return result;
       }
@@ -94,24 +94,24 @@ let _produceResult = function(matchedIntent, stateAccessor, stateSelectors, resp
     else if(typeof responderSpec.result.directValues !== "undefined"){
       let directValues = responderSpec.result.directValues;
       if(directValues.pickMethod === "random"){
-//      console.log("_produceResult, random");
+        //      console.log("_produceResult, random");
         if(typeof directValues.values !== "undefined" && Array.isArray(directValues.values)){
           let randomIndex = Math.floor(Math.random() * directValues.values.length);
           return directValues.values[randomIndex];
         }
       }
       else if(directValues.pickMethod === "randomDoNotRepeat"){
-//      console.log("_produceResult, randomDoNotRepeat");
+        //      console.log("_produceResult, randomDoNotRepeat");
         if(typeof directValues.values !== "undefined" && Array.isArray(directValues.values) &&
           typeof directValues.repeatSelector !== "undefined" && directValues.repeatSelector !== null){
 
           let updatedStateSelectors = [].concat(stateSelectors);
           updatedStateSelectors.push(directValues.repeatSelector);
           let usedValues = stateAccessor.getStateChain(updatedStateSelectors);
-//        console.log("usedValues: ", JSON.stringify(usedValues));
+          //        console.log("usedValues: ", JSON.stringify(usedValues));
           if(typeof usedValues === "undefined" || Array.isArray(usedValues) !== true){
             usedValues = [];
-//          console.log("setting usedValues to empty array: ", JSON.stringify(usedValues));
+            //          console.log("setting usedValues to empty array: ", JSON.stringify(usedValues));
           }
           let unusedValues = [];
           let stringifiedUsedValues = [];
@@ -120,7 +120,7 @@ let _produceResult = function(matchedIntent, stateAccessor, stateSelectors, resp
           }
           for(let i = 0; i < directValues.values.length; i++){
             if(stringifiedUsedValues.indexOf(JSON.stringify(directValues.values[i])) < 0){
-//            console.log("adding to unusedValues: ", JSON.stringify(directValues.values[i]));
+              //            console.log("adding to unusedValues: ", JSON.stringify(directValues.values[i]));
               unusedValues.push(directValues.values[i]);
             }
           }
@@ -129,7 +129,7 @@ let _produceResult = function(matchedIntent, stateAccessor, stateSelectors, resp
             unusedValues = unusedValues.concat(directValues.values);
             usedValues = [];
           }
-//        console.log("unusedValues: ", JSON.stringify(unusedValues));
+          //        console.log("unusedValues: ", JSON.stringify(unusedValues));
           let randomIndex = Math.floor(Math.random() * unusedValues.length);
           let returnValue = unusedValues[randomIndex];
           if(typeof returnValue !== "undefined" && returnValue !== null){
@@ -149,106 +149,102 @@ let _combineResponses = function(response1, response2, combineRule){
 //  console.log("_combineResponses, 1.1, response2: ", JSON.stringify(response2, null, 2));
 //  console.log("_combineResponses, 1.2, combineRule: ", combineRule);
   if(typeof response2 === "undefined" || response2 === null){
-//    console.log("_combineResponses, 2");
+    //    console.log("_combineResponses, 2");
     return response2;
   }
-//  console.log("_combineResponses, 3, combineRule: " + combineRule);
+  //  console.log("_combineResponses, 3, combineRule: " + combineRule);
   if(typeof combineRule === "undefined" || combineRule === null){
-//    console.log("_combineResponses, 4");
+    //    console.log("_combineResponses, 4");
     combineRule = "mergeReplace";
   }
   let returnValue;
   switch(combineRule){
-    case "ignore":
-      return response1;
-      break;
-    case "mergeReplace":
-//      console.log("_combineResponses, 5");
-      returnValue = JSON.parse(JSON.stringify(response1));
-      for (let property in response2) {
-        if (response2.hasOwnProperty(property)) {
+  case "ignore":
+    return response1;
+  case "mergeReplace":
+    //      console.log("_combineResponses, 5");
+    returnValue = JSON.parse(JSON.stringify(response1));
+    for (let property in response2) {
+      if (response2.hasOwnProperty(property)) {
+        returnValue[property] = response2[property];
+      }
+    }
+    return returnValue;
+  case "mergeAppend":
+    //      console.log("_combineResponses, 6");
+    returnValue = JSON.parse(JSON.stringify(response1));
+    //      console.log("_combineResponses, 6.1");
+    for (var property in response2) {
+      if (response2.hasOwnProperty(property)) {
+        let existingValue = response1[property];
+        let newValue = response2[property];
+        if(typeof existingValue === "undefined" || existingValue === null){
           returnValue[property] = response2[property];
         }
-      }
-      return returnValue;
-      break;
-    case "mergeAppend":
-//      console.log("_combineResponses, 6");
-      returnValue = JSON.parse(JSON.stringify(response1));
-//      console.log("_combineResponses, 6.1");
-      for (var property in response2) {
-        if (response2.hasOwnProperty(property)) {
-          let existingValue = response1[property];
-          let newValue = response2[property];
-          if(typeof existingValue === "undefined" || existingValue === null){
-            returnValue[property] = response2[property];
+        else if(typeof newValue === "undefined" || newValue === null){
+          delete returnValue[property];
+        }
+        else if(typeof existingValue === "string"){
+          if(property === "text"){
+            returnValue[property] = existingValue + "  " + response2[property];
           }
-          else if(typeof newValue === "undefined" || newValue === null){
-            delete returnValue[property];
-          }
-          else if(typeof existingValue === "string"){
-            if(property === "text"){
-              returnValue[property] = existingValue + "  " + response2[property];
-            }
-            else if(property === "ssml"){
-              if(/<\/speak>\s*$/ig.test(existingValue) && /^\s*<speak>/ig.test(newValue.toString())){
-                // Strip "inner" speak tags, and concatenate.
-                try{
-                  returnValue[property] = /(.*)(?:<\/speak>\s*$)/ig.exec(existingValue)[1] + "  " + /(?:^\s*<speak>)(.*)/ig.exec(newValue.toString())[1];
-                }
-                catch(e){
-                  returnValue[property] = existingValue + "  " + newValue.toString();
-                }
-              }
-            }
-            else {
-              // Catch all string concatenation
-              returnValue[property] = existingValue + newValue.toString();
-            }
-          }
-          else if(Array.isArray(existingValue)){
-            if(Array.isArray(newValue)){
-              // concatenate arrays
-              returnValue[property] = existingValue.concat(newValue);
-            }
-            else {
-              // add to an array
-              returnValue[property] = existingValue.concat([newValue]);
-            }
-          }
-          else if(typeof existingValue === "object" && typeof newValue === "object"){
-            if(property === "card"){
+          else if(property === "ssml"){
+            if(/<\/speak>\s*$/ig.test(existingValue) && /^\s*<speak>/ig.test(newValue.toString())){
+              // Strip "inner" speak tags, and concatenate.
               try{
-                delete returnValue["card"];
+                returnValue[property] = /(.*)(?:<\/speak>\s*$)/ig.exec(existingValue)[1] + "  " + /(?:^\s*<speak>)(.*)/ig.exec(newValue.toString())[1];
               }
               catch(e){
-                // ignore
+                returnValue[property] = existingValue + "  " + newValue.toString();
               }
-              returnValue["cards"] = {existingValue, newValue};
-            }
-            else {
-              returnValue[property] = {existingValue, newValue};
             }
           }
           else {
+            // Catch all string concatenation
+            returnValue[property] = existingValue + newValue.toString();
+          }
+        }
+        else if(Array.isArray(existingValue)){
+          if(Array.isArray(newValue)){
+            // concatenate arrays
+            returnValue[property] = existingValue.concat(newValue);
+          }
+          else {
+            // add to an array
+            returnValue[property] = existingValue.concat([newValue]);
+          }
+        }
+        else if(typeof existingValue === "object" && typeof newValue === "object"){
+          if(property === "card"){
             try{
-              returnValue[property] = existingValue + newValue;
+              delete returnValue["card"];
             }
             catch(e){
-              returnValue[property] = existingValue.toString() + newValue.toString();
+              // ignore
             }
+            returnValue["cards"] = {existingValue, newValue};
+          }
+          else {
+            returnValue[property] = {existingValue, newValue};
+          }
+        }
+        else {
+          try{
+            returnValue[property] = existingValue + newValue;
+          }
+          catch(e){
+            returnValue[property] = existingValue.toString() + newValue.toString();
           }
         }
       }
-      return returnValue;
-
-      break;
-    case "setTo":
-//      console.log("_combineResponses, 7");
-    default:
-//      console.log("_combineResponses, 8");
-      return response2;
-      break;
+    }
+    return returnValue;
+  case "setTo":
+    //      console.log("_combineResponses, 7");
+    return response2;
+  default:
+    //      console.log("_combineResponses, 8");
+    return response2;
   }
 };
 
