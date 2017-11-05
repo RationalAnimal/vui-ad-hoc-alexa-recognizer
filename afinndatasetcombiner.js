e241d7a752906726ff7447432ab9969ffe308eb1
RationalAnimal <RationalAnimal@users.noreply.github.com>
2017-11-05 14:10:51 -0500

    yet more #2144 related code

--- afinndatasetcombiner.js
+++ afinndatasetcombiner.js
@@ -26,84 +26,117 @@
 "use strict";
 let fs = require("fs");// eslint-disable-line no-unused-vars
 
-let usage = function(){// eslint-disable-line no-unused-vars
+let usage = function(){
   console.log("Usage: node " + process.argv[1] + " <sampleutterance.txt>");
   console.log("  -i --input InputFileNames - specify a list of input file names.");
   console.log("  -o --output OutputFileName specify output combined file name.");
 };
 
+let parsingFiles = "none";
+let inputFileNames = [];
+let outputFileName;
+let continueProcessing = true;
+for(let i = 2; i < process.argv.length; i ++){
+  if(process.argv[i] === "-i" || process.argv[i] === "--input"){
+    parsingFiles = "input";
+  }
+  else if(process.argv[i] === "-o" || process.argv[i] === "--output"){
+    parsingFiles = "output";
+  }
+  else {
+    switch(parsingFiles){
+    case "input":
+      inputFileNames.push(process.argv[i]);
+      break;
+    case "output":
+      outputFileName = process.argv[i];
+      break;
+    default:
+      usage();
+      continueProcessing = false;
+      break;
+    }
+  }
+}
+
+if(typeof outputFileName === "undefined" || inputFileNames.length === 0){
+  usage();
+  continueProcessing = false;
+}
 let dataSet = {"scoredWords": []};
 let precomputed = false;
 let customArgs;
-if(typeof customArgs.ratingDataSetFiles !== "undefined" && customArgs.ratingDataSetFiles !== null){
-  for(let i = 0; i < customArgs.ratingDataSetFiles.length; i++){
-    let scratchDataSet = require(customArgs.ratingDataSetFiles[i]);
-    dataSet.scoredWords = dataSet.scoredWords.concat(scratchDataSet.scoredWords);
-    precomputed = false;
-  }
-}
-
-if(precomputed === false){
-  // Pre-compute word count to improve performance
-  for(let i = 0; i < dataSet.scoredWords.length; i++){
-    let split = dataSet.scoredWords[i].word.split(/\s+/);
-    dataSet.scoredWords[i].wordCount = split.length;
+if(continueProcessing){
+  if(typeof customArgs.ratingDataSetFiles !== "undefined" && customArgs.ratingDataSetFiles !== null){
+    for(let i = 0; i < customArgs.ratingDataSetFiles.length; i++){
+      let scratchDataSet = require(customArgs.ratingDataSetFiles[i]);
+      dataSet.scoredWords = dataSet.scoredWords.concat(scratchDataSet.scoredWords);
+      precomputed = false;
+    }
   }
 
-  // Sort the array by how many words are in a "word", then the "word" itself, in descending order
-  dataSet.scoredWords.sort(
-    function(a,b){
-      if(a.wordCount === b.wordCount){
-        if (a.word < b.word){
-          return 1;
+  if(precomputed === false){
+    // Pre-compute word count to improve performance
+    for(let i = 0; i < dataSet.scoredWords.length; i++){
+      let split = dataSet.scoredWords[i].word.split(/\s+/);
+      dataSet.scoredWords[i].wordCount = split.length;
+    }
+
+    // Sort the array by how many words are in a "word", then the "word" itself, in descending order
+    dataSet.scoredWords.sort(
+      function(a,b){
+        if(a.wordCount === b.wordCount){
+          if (a.word < b.word){
+            return 1;
+          }
+          else if (a.word > b.word){
+            return -1;
+          }
+          else {
+            // This should never happen here, but technically is possible.
+            return 0;
+          }
         }
-        else if (a.word > b.word){
+        if(a.word.startsWith(b.word)){
           return -1;
         }
-        else {
-          // This should never happen here, but technically is possible.
-          return 0;
+        if(b.word.startsWith(a.word)){
+          return 1;
         }
+        return (b.wordCount - a.wordCount);
       }
-      if(a.word.startsWith(b.word)){
-        return -1;
-      }
-      if(b.word.startsWith(a.word)){
-        return 1;
-      }
-      return (b.wordCount - a.wordCount);
-    }
-  );
+    );
 
-  // Remove duplicated words
-  for(let i = 0; i < dataSet.scoredWords.length - 1; i++){
-    if(dataSet.scoredWords[i].word === dataSet.scoredWords[i+1].word){
-      dataSet.scoredWords.splice(i, 1);
-      i--;
+    // Remove duplicated words
+    for(let i = 0; i < dataSet.scoredWords.length - 1; i++){
+      if(dataSet.scoredWords[i].word === dataSet.scoredWords[i+1].word){
+        dataSet.scoredWords.splice(i, 1);
+        i--;
+      }
     }
   }
-}
 
-let scratchUtterance = "";
-for(let i = 0; i < dataSet.scoredWords.length; i++){
-  let regExp;
-  if(dataSet.scoredWords[i].caseInsensitive === false){
-    regExp = new RegExp(dataSet.scoredWords[i].regExpString, "g");
-  }
-  else {
-    regExp = new RegExp(dataSet.scoredWords[i].regExpString, "ig");
-  }
-  let matchResult; // eslint-disable-line no-unused-vars
-  let matchFound = false;
-  while(matchResult = regExp.exec(scratchUtterance)) {// eslint-disable-line no-cond-assign
-    //console.log("matchResult: ", JSON.stringify(matchResult));
-    matchFound = true;
-  }
-  // Remove the matched string make sure it won't be matched on again by substrings.
-  if(matchFound){
-    //console.log("about to replace matched: ", JSON.stringify(dataSet.scoredWords[i]));
-    //console.log("scratchUtterance before replace: " + scratchUtterance);
-    scratchUtterance = scratchUtterance.replace(new RegExp(dataSet.scoredWords[i].regExpString), " #unmatchable# ");
-    //console.log("scratchUtterance after replace: " + scratchUtterance);
+  let scratchUtterance = "";
+  for(let i = 0; i < dataSet.scoredWords.length; i++){
+    let regExp;
+    if(dataSet.scoredWords[i].caseInsensitive === false){
+      regExp = new RegExp(dataSet.scoredWords[i].regExpString, "g");
+    }
+    else {
+      regExp = new RegExp(dataSet.scoredWords[i].regExpString, "ig");
+    }
+    let matchResult; // eslint-disable-line no-unused-vars
+    let matchFound = false;
+    while(matchResult = regExp.exec(scratchUtterance)) {// eslint-disable-line no-cond-assign
+      //console.log("matchResult: ", JSON.stringify(matchResult));
+      matchFound = true;
+    }
+    // Remove the matched string make sure it won't be matched on again by substrings.
+    if(matchFound){
+      //console.log("about to replace matched: ", JSON.stringify(dataSet.scoredWords[i]));
+      //console.log("scratchUtterance before replace: " + scratchUtterance);
+      scratchUtterance = scratchUtterance.replace(new RegExp(dataSet.scoredWords[i].regExpString), " #unmatchable# ");
+      //console.log("scratchUtterance after replace: " + scratchUtterance);
+    }
   }
 }
