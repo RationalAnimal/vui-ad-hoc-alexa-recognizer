b2884bfb692282e83fed1013a25c8efdf811651e
RationalAnimal <RationalAnimal@users.noreply.github.com>
2017-11-06 02:15:24 -0500

    more emojis

--- test/recognizer.js
+++ test/recognizer.js
@@ -4098,6 +4098,20 @@ describe("vui-ad-hoc-alexa-recognizer", function() {
         });
     });
 
+    it("verify sentiment analysis is computed correctly for a double emojis", function() {
+      let result = recognizer.Recognizer.matchText("AFINNEMOTE :')))))) :'))");
+      expect(result).to.eql(
+        {
+          "name": "AfinnEmoticonIntent",
+          "slots": {},
+          "sentiment": {
+            "AFINN": {
+              "score": 7
+            }
+          }
+        });
+    });
+
   });
 
   /*
