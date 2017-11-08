a9dc757c035041fddf782092efe9cb896b21a82a
RationalAnimal <RationalAnimal@users.noreply.github.com>
2017-11-07 20:45:32 -0500

    emoji testing

--- test/recognizer.js
+++ test/recognizer.js
@@ -4574,6 +4574,19 @@ describe("vui-ad-hoc-alexa-recognizer", function() {
         });
     });
 
+    it("verify sentiment analysis is computed correctly for a multiple emoji", function() {
+      let result = recognizer.Recognizer.matchText("AFINNEMOTE :^( :^(( :^(((((( :^((");
+      expect(result).to.eql(
+        {
+          "name": "AfinnEmoticonIntent",
+          "slots": {},
+          "sentiment": {
+            "AFINN": {
+              "score": -12
+            }
+          }
+        });
+    });
 
   });
 
