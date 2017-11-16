2915bf2d6ebdd25925bcd7ec74eadb5e0c2b7da9
RationalAnimal <RationalAnimal@users.noreply.github.com>
2017-11-15 19:20:58 -0500

    emoji testing

--- test/recognizer.js
+++ test/recognizer.js
@@ -3067,7 +3067,7 @@ describe("vui-ad-hoc-alexa-recognizer", function() {
         });
     });
 
-    it("verify sentiment analysis is computed correctly for double emoji", function() {
+    it("verify sentiment analysis is computed correctly for triple emoji", function() {
       let result = recognizer.Recognizer.matchText("AFINNEMOTE ]: ]]]]]]: ]]:");
       expect(result).to.eql(
         {
@@ -3076,6 +3076,20 @@ describe("vui-ad-hoc-alexa-recognizer", function() {
           "sentiment": {
             "AFINN": {
               "score": -9
+            }
+          }
+        });
+    });
+
+    it("verify sentiment analysis is computed correctly for multiple emoji", function() {
+      let result = recognizer.Recognizer.matchText("AFINNEMOTE ]: ]]: ]]]]]]: ]]:");
+      expect(result).to.eql(
+        {
+          "name": "AfinnEmoticonIntent",
+          "slots": {},
+          "sentiment": {
+            "AFINN": {
+              "score": -12
             }
           }
         });
