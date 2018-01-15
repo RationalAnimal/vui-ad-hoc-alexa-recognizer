3361ee37df3d9a27b67bd13d8d65d30afc4e552b
RationalAnimal <RationalAnimal@users.noreply.github.com>
2018-01-14 21:53:14 -0500

    added emoticon

--- test/recognizer.js
+++ test/recognizer.js
@@ -9896,6 +9896,20 @@ describe("vui-ad-hoc-alexa-recognizer", function() {
     });
 
     it("verify sentiment analysis is computed correctly for single emoji", function() {
+      let result = recognizer.Recognizer.matchText("AFINNEMOTE ='|");
+      expect(result).to.eql(
+        {
+          "name": "AfinnEmoticonIntent",
+          "slots": {},
+          "sentiment": {
+            "AFINN": {
+              "score": -1
+            }
+          }
+        });
+    });
+
+    it("verify sentiment analysis is computed correctly for single emoji", function() {
       let result = recognizer.Recognizer.matchText("AFINNEMOTE ;']]");
       expect(result).to.eql(
         {
