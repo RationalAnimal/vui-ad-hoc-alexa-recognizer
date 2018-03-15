cd0df34f6a2b8d4f435a3af05f25d8289abee2d1
RationalAnimal <RationalAnimal@users.noreply.github.com>
2018-03-15 07:10:18 -0400

    added two emoticons

--- test/recognizer.js
+++ test/recognizer.js
@@ -20551,6 +20551,32 @@ describe("vui-ad-hoc-alexa-recognizer", function() {
           }
         });
     });
+    it("verify sentiment analysis is computed correctly for a single emoji with commonality threshold", function() {
+      let result = recognizer.Recognizer.matchText("AFINNEMOTECOMMON ))';");
+      expect(result).to.eql(
+        {
+          "name": "AfinnEmoticonMostCommonIntent",
+          "slots": {},
+          "sentiment": {
+            "AFINN": {
+              "score": 0
+            }
+          }
+        });
+    });
+    it("verify sentiment analysis is computed correctly for a single emoji with commonality threshold", function() {
+      let result = recognizer.Recognizer.matchText("AFINNEMOTECOMMON )))';");
+      expect(result).to.eql(
+        {
+          "name": "AfinnEmoticonMostCommonIntent",
+          "slots": {},
+          "sentiment": {
+            "AFINN": {
+              "score": 0
+            }
+          }
+        });
+    });
 
   });
 
