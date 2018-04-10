60df0bd710c56d454e91a2bd76f5a406ecd0a3c7
RationalAnimal <RationalAnimal@users.noreply.github.com>
2018-04-10 07:04:11 -0400

    resolved issue #3318

--- test/recognizer.js
+++ test/recognizer.js
@@ -22413,6 +22413,28 @@ describe("domain parsing", function() {
       );
     });
 
+    it("verify multi recognizer domain with is not white space test match criteria and a basic accessor parses", function () {
+      let domain = require("../test/nulltestdomain/domain.json");
+
+      let applicationState = {"somevalue": "abc123  "};
+      let basicStateAccessor = require("../builtinstateaccessors/basic.js");
+      let stateAccessor = new basicStateAccessor(applicationState);
+
+      let result = recognizer.Recognizer.matchDomain("testing whether we match on an is not white space testing criteria", domain, stateAccessor);
+      expect(result).to.eql(
+        {
+          "match":
+            {
+              "name": "IsNotWhiteSpaceMatchingCriteriaTestingIntent",
+              "slots": {}
+            },
+          "result": {
+            "text": "Is not white space test recognizer used"
+          }
+        }
+      );
+    });
+
     it("verify multi recognizer domain with non default match criteria and a state sub select accessor parses", function () {
       let domain = require("../test/blahblahdomain/blahblahdomain.json");
       let applicationState = {
