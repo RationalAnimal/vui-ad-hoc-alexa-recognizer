104d76fc8089ff3a301f8a12aa64bc2c53cb8306
RationalAnimal <RationalAnimal@users.noreply.github.com>
2018-04-13 20:09:04 -0400

    added isUpperCase: false to issue #3319

--- index.js
+++ index.js
@@ -1635,7 +1635,8 @@ let _checkStateMatchCriteria = function(state, stateAccessor, applicationState){
         ((state.matchCriteria.isAlphaNumeric === false) && (typeof stateAccessor.getState(state.matchCriteria.selector) === "string") && (/^[a-zA-Z0-9]+$/.test(stateAccessor.getState(state.matchCriteria.selector)) === false)) || // Matching on the value being at least partially non alpha numeric
         ((state.matchCriteria.isWhiteSpace === true) && (typeof stateAccessor.getState(state.matchCriteria.selector) === "string") && (/^\s+$/.test(stateAccessor.getState(state.matchCriteria.selector))  === true)) || // Matching on the value being all whitespace
         ((state.matchCriteria.isWhiteSpace === false) && (typeof stateAccessor.getState(state.matchCriteria.selector) === "string") && (/^\s+$/.test(stateAccessor.getState(state.matchCriteria.selector))  === false)) || // Matching on the value being at least partially non whitespace
-        ((state.matchCriteria.isUpperCase === true) && (typeof stateAccessor.getState(state.matchCriteria.selector) === "string") && (/^[A-Z]+$/.test(stateAccessor.getState(state.matchCriteria.selector)) === true)) // Matching on the value being all upper case alpha
+        ((state.matchCriteria.isUpperCase === true) && (typeof stateAccessor.getState(state.matchCriteria.selector) === "string") && (/^[A-Z]+$/.test(stateAccessor.getState(state.matchCriteria.selector)) === true)) || // Matching on the value being all upper case alpha
+        ((state.matchCriteria.isUpperCase === false) && (typeof stateAccessor.getState(state.matchCriteria.selector) === "string") && (/^[A-Z]+$/.test(stateAccessor.getState(state.matchCriteria.selector))  === false)) // Matching on the value being at least partially non upper case alpha
       )
     )
   ){
