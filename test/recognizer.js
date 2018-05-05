5b04d58847a13f1f1322cbdfc2a387f4087405ce
RationalAnimal <RationalAnimal@users.noreply.github.com>
2018-05-05 12:47:58 -0400

    added another emoticon

--- test/recognizer.js
+++ test/recognizer.js
@@ -22636,6 +22636,19 @@ describe("vui-ad-hoc-alexa-recognizer", function() {
           }
         });
     });
+    it("verify sentiment analysis is computed correctly for a single emoji with commonality threshold", function() {
+      let result = recognizer.Recognizer.matchText("AFINNEMOTECOMMON ]]]]]]=");
+      expect(result).to.eql(
+        {
+          "name": "AfinnEmoticonMostCommonIntent",
+          "slots": {},
+          "sentiment": {
+            "AFINN": {
+              "score": 0
+            }
+          }
+        });
+    });
 
 
   });
