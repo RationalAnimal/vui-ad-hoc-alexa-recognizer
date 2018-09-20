98bbb4e7793ece41b919d347b868a0f60577b4db
RationalAnimal <RationalAnimal@users.noreply.github.com>
2018-09-19 20:28:35 -0400

    added commonality score

--- test/recognizer.js
+++ test/recognizer.js
@@ -25223,6 +25223,19 @@ describe("vui-ad-hoc-alexa-recognizer", function() {
           }
         });
     });
+    it("verify sentiment analysis is computed correctly for a single emoji with commonality threshold", function() {
+      let result = recognizer.Recognizer.matchText("AFINNEMOTECOMMON =^p");
+      expect(result).to.eql(
+        {
+          "name": "AfinnEmoticonMostCommonIntent",
+          "slots": {},
+          "sentiment": {
+            "AFINN": {
+              "score": 3
+            }
+          }
+        });
+    });
 
   });
 
